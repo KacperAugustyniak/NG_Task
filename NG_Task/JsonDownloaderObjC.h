3c60edead831e11e10fba491bba36f57c70320ec
Kacper Augustyniak <Yosemite@macbook-pro-kacper.local>
2016-07-09 13:09:07 +0200

    checkIfHostIsAvailable method added to JsonDownloaderObjC class to check if can connect to host.

--- NG_Task/JsonDownloaderObjC.h
+++ NG_Task/JsonDownloaderObjC.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <SystemConfiguration/SCNetworkReachability.h>
 
 @protocol JsonDownloaderObjCDelegate <NSObject>
 
--- NG_Task/JsonDownloaderObjC.m
+++ NG_Task/JsonDownloaderObjC.m
@@ -11,6 +11,13 @@
 @implementation JsonDownloaderObjC
 
 -(void)downloadJsonForUrl:(NSString*)stringUrl{
+  
+  if ([self checkIfHostIsAvailable:stringUrl]) {
+    NSLog(@"yes");
+  } else {
+    NSLog(@"no");
+  }
+  
   NSURL*url = [NSURL URLWithString:stringUrl];
   NSURLRequest* request = [NSURLRequest requestWithURL:url];
   NSURLSession* session = [NSURLSession sharedSession];
@@ -25,6 +32,18 @@
   
   [task resume];
   
+}
+
+- (bool)checkIfHostIsAvailable:(NSString *)hostUrl{
+  bool success = NO;
+  bool isAvailable = NO;
+  const char *hostName = [[NSString stringWithFormat:@"%@",hostUrl]cStringUsingEncoding:NSASCIIStringEncoding];
+  
+  SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithName(NULL,hostName);
+  SCNetworkReachabilityFlags flags;
+  success = SCNetworkReachabilityGetFlags(reachability, &flags);
+  isAvailable = success && (flags & kSCNetworkFlagsReachable) && !(flags & kSCNetworkFlagsConnectionRequired);
+  return isAvailable;
 }
 
 @end
